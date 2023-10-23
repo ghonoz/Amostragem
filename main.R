19bd697f97c7ad51615850af0ce7ed97f1569e98
Gabriel <gabrielrubinho05@gmail.com>
2023-10-22 23:16:52 -0300

    comentário

---
+++ main.R
@@ -0,0 +1,29 @@
+library(janitor)
+library(ggplot2)
+
+df <- read.csv('questionario_alcool.csv', 
+               sep = ',', 
+               fileEncoding = 'UTF-8',
+               header = TRUE)
+
+df <- df %>% janitor::clean_names()
+
+df1 <- df[,-1]
+
+antigo_nomes <- as.vector(colnames(df1))
+novos_nomes <- c('curso', 'atividade_remunerada', 'tipo_atividade_remunerada',
+                 'ingere_alcool', 'se_fornao_ja_ingeriu', 'bebe_onde', 
+                 'sintomas', 'frequencia', 'ja_teve_problemas', 'gasto_mensal', 
+                 'ja_tentou_parar', 'o_que_leva_a_beber')
+
+
+df2 <- df1 %>% 
+  dplyr::rename_with(
+    ~novos_nomes, all_of(antigo_nomes)
+  )
+
+
+
+
+ggplot(df2, aes(x = curso)) + 
+  geom_bar()
